4d0d7bb7e1e1736305d0a274576f239bffe66a48
vveitas <vveitas@gmail.com>
2016-01-10 13:42:43 +0100

    removed redundant scripts directory

--- scripts/push_to_website.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-cp -r ../_book/* ../../../future-internet/
-
-cd ../../../future-internet
-git add .
-git commit -m 'updated the book'
-git push -u origin gh-pages
